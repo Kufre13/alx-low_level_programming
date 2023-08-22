ba48f2be743b0e30caac8847efa4c9bb38ffdc79
Kufre13 <Kufak1510@gmail.com>
2023-08-22 09:30:12 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,7 +5,6 @@
 int _putchar(char c);
 void reset_to_98(int *n);
 void print_array(int *a, int *n);
-void print_array(int *a, int n);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
